e99f6792486d01d2f53f751841d72371d801c569
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2023-09-08 21:22:38 -0700

    added background gradient

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import Nav from './Nav';
 import Home from './Home';
 
+import './styles/style.scss';
+
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <StrictMode>
