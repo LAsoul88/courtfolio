8e7a3289652c89af471e539039d6a7fdca782c8b
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2023-09-12 15:38:08 -0700

    added basic text and icons with links

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 
+import { GitHub, LinkedIn } from './images/images';
+
+import './styles/Home.scss';
+
 const Home = () => {
   return (
-    <h1></h1>
+    <>
+      <h1></h1>
+      <main>
+        <section className="greeting">
+          <p className="hello">Hello</p>
+          <p className="">I'm Courtney Kakebeen</p>
+          <p></p>
+        </section>
+        <section className="links">
+          <a href="https://github.com/LAsoul88" target="_blank"><GitHub fill="black" /></a>
+          <a href="https://www.linkedin.com/in/courtneykakebeen/" target="_blank"><LinkedIn fill="black"
+          /></a>
+        </section>
+      </main>
+    </>
   );
 }
 
