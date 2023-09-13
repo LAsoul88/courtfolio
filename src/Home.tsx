c0f188ed1be78c233752f4900323d0fed91a8b8e
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2023-09-12 21:31:05 -0700

    working to add on hover effect to buttons, typing refs next

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState, useRef, MouseEvent } from 'react';
 
 import { GitHub, LinkedIn } from './images/images';
 
 import './styles/Home.scss';
 
 const Home = () => {
+  const [liHover, setLiHover] = useState(false);
+  const [gitHover, setGitHover] = useState(false);
+
+  const liRef = useRef(null);
+  const gitRef = useRef();
+
+  const toggleHover = ({ target, currentTarget }: MouseEvent<HTMLAnchorElement>): void => {
+    console.log(target)
+  }
+
   return (
     <>
       <h1></h1>
@@ -15,7 +25,15 @@ const Home = () => {
           <p>Front End Developer</p>
         </section>
         <section className="links">
-          <a href="https://github.com/LAsoul88" target="_blank"><GitHub fill="black" /></a>
+          <a 
+            href="https://github.com/LAsoul88" 
+            target="_blank"
+            ref={gitRef}
+            onMouseEnter={toggleHover}
+            onMouseLeave={() => {
+              console.log("Event:MouseLeave")
+            }}
+          ><GitHub fill="black" /></a>
           <a href="https://www.linkedin.com/in/courtneykakebeen/" target="_blank"><LinkedIn fill="black"
           /></a>
         </section>
