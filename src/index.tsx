d9cbc88f337f510d0ff58c4885daa370b4cb7556
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2023-09-13 20:55:25 -0700

    added routes, enabled navigation by url bar

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,26 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './Nav';
 import Home from './Home';
+import About from './About';
+import Projects from './Projects';
+import Contact from './Contact';
 
 import './styles/style.scss';
+
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <StrictMode>
-    <Nav />
-    <Home />
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
