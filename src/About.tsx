bec32262703d360910be057b47758d4d589bb85f
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2023-09-19 14:55:41 -0700

    structuring about page

--- src/About.tsx
+++ src/About.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
+import './styles/About.scss';
+
 const About = () => {
   return (
-    <></>
+    <>
+      <h2>About Me</h2>
+      <section>
+        About
+      </section>
+      <section>
+        Skills
+      </section>
+    </>
   )
 }
 
