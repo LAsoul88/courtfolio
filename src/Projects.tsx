0bb1f0f81db991f5fa29641adbe818b1412f5d3f
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2023-10-03 14:41:02 -0700

    set up scaffolding for project page

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 
+import { projectList } from './util/projectList';
+
+import './styles/Projects';
+
 const Projects = () => {
   return (
-    <></>
+    <>
+      <h2>Projects</h2>
+      <section className="projects-container">
+        { projectList.map(project => {
+          return (
+            <div className="project-container">
+              <h3>
+                {project.name}
+              </h3>
+              <p className="description">{project.description}</p>
+            </div>
+          )
+        })}
+      </section>
+    </>
   )
 }
 
