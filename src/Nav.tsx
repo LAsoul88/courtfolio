c6981ddba059f0a2f62d64722567d56ab1ca04f7
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2023-10-16 22:58:21 -0700

    setting up resume download button in nav

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import './styles/Nav';
 
 const Nav = () => {
+  const resume = require('./util/Courtney_Kakebeen_Full_Stack_Engineer.docx');
+
   return (
     <header>
       <img src="" alt="logo" />
@@ -13,7 +15,10 @@ const Nav = () => {
           <li><Link to="/about" className="link">About</Link></li>
           <li><Link to="/projects" className="link">Projects</Link></li>
           <li><Link to="/contact" className="link">Contact</Link></li>
-          <button>Resume</button>
+          <a 
+            href={resume} download 
+            target="_blank"
+          >Resume</a>
         </ul>
       </nav>
     </header>
